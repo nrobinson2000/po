114069b1fd757f741251419a14808fcad61d4701
Nathan Robinson <nrobinson2000@me.com>
2018-03-26 21:30:46 -0400

    Added regenerate support at 21:30 of 2018-03-26

--- po.rb
+++ po.rb
@@ -19,8 +19,8 @@ class Newpo < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage 'https://po-util.com'
   head 'https://github.com/nrobinson2000/homebrew-NewPo', :using => :git
-  url 'https://github.com/nrobinson2000/po/archive/0.0.9.tar.gz'
-  sha256 'e490229f95c2bc1f79d2452da1bfb970ed08477c348705978b5dc3fcbf18b694'
+  url 'https://github.com/nrobinson2000/po/archive/0.1.0.tar.gz'
+  sha256 '5903e4592bd5ff69633039d5af436a0c2812c4f67b6e93f42bb717bdf283fd28'
 
   def install
     bin.install 'po'
