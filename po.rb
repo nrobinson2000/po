c0e3e40142de11dd7833bfc89872914fc447db17
Nathan Robinson <nrobinson2000@me.com>
2018-03-27 22:25:48 -0400

    https://github.com/nrobinson2000/homebrew-po/issues/7 at 22:25 of 2018-03-27

--- po.rb
+++ po.rb
@@ -19,8 +19,8 @@ class Newpo < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage 'https://po-util.com'
   head 'https://github.com/nrobinson2000/homebrew-NewPo', :using => :git
-  url 'https://github.com/nrobinson2000/po/archive/0.1.0.tar.gz'
-  sha256 '5903e4592bd5ff69633039d5af436a0c2812c4f67b6e93f42bb717bdf283fd28'
+  url 'https://github.com/nrobinson2000/po/archive/0.1.1.tar.gz'
+  sha256 '0c54db7ba02816a477accc5c931d16f669641ad940caac0fe658a9953d08c8b9'
 
   def install
     bin.install 'po'
