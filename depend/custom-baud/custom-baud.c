d5977c2bb447bea1fa3df9e2705ec9bacd84cfee
Nathan Robinson <nrobinson2000@me.com>
2018-04-11 16:30:11 -0400

    [skip ci] at 16:30 of 2018-11-04

--- depend/custom-baud/custom-baud.c
+++ depend/custom-baud/custom-baud.c
@@ -1,7 +1,23 @@
 /*
- * Allows to set arbitrary speed for the serial device on Linux.
+   ______    ______
+  /      \  /      \
+ /██████  |/██████  |  The Future of po-util:
+ ██ |  ██ |██ |  ██ |
+ ██ |__██ |██ \__██ |      Refactored,
+ ██    ██/ ██    ██/       Modular,
+ ███████/   ██████/        Cross-Compatible
+ ██ |
+ ██ |
+ ██/  https://po-util.com
+
+po - The Ultimate Local Particle Experience for Linux and macOS
+Copyright (C) 2018 Nathan D. Robinson
+
+custom-baud
+ * Allows to set arbitrary baud rate for serial devices on Linux.
  * stty allows to set only predefined values: 9600, 19200, 38400, 57600, 115200, 230400, 460800.
  */
+ 
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -11,25 +27,24 @@
 int main(int argc, char* argv[]) {
 
     if (argc != 3) {
-        printf("%s device speed\n\nSet speed for a serial device.\nFor instance:\n    %s /dev/ttyUSB0 75000\n", argv[0], argv[0]);
+        printf("%s device rate\n\nSet baud rate for a serial device.\nFor instance:\n    %s /dev/ttyACM0 14400\n", argv[0], argv[0]);
         return -1;
     }
 
     int fd = open(argv[1], O_RDONLY);
-
-    int speed = atoi(argv[2]);
+    int rate = atoi(argv[2]);
 
     struct termios2 tio;
     ioctl(fd, TCGETS2, &tio);
     tio.c_cflag &= ~CBAUD;
     tio.c_cflag |= BOTHER;
-    // tio.c_ispeed = speed;
-    tio.c_ospeed = speed;
+    // tio.c_ispeed = rate;
+    tio.c_ospeed = rate;
     int r = ioctl(fd, TCSETS2, &tio);
     close(fd);
 
     if (r == 0) {
-        printf("Changed successfully.\n");
+        printf("Set %s to %d successfully.\n", argv[1], rate);
     } else {
         perror("ioctl");
     }
