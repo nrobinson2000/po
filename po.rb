b4102e41291042de73e5c1cd18ec74ccfa1d4e7d
Nathan Robinson <nrobinson2000@me.com>
2018-04-23 23:39:37 -0400

    [skip ci] at 23:39 of 2018-04-23

--- po.rb
+++ po.rb
@@ -1,54 +1,29 @@
-#     ______    ______
-#    /      \  /      \
-#   /██████  |/██████  |  The Future of po-util:
-#   ██ |  ██ |██ |  ██ |
-#   ██ |__██ |██ \__██ |      Refactored,
-#   ██    ██/ ██    ██/       Modular,
-#   ███████/   ██████/        Cross-Compatible
-#   ██ |
-#   ██ |
-#   ██/  https://po-util.com
-
-# po - The Ultimate Local Particle Experience for Linux and macOS
-# Copyright (C) 2018 Nathan D. Robinson
-
 class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
-  homepage 'https://po-util.com'
-  url 'https://github.com/nrobinson2000/po/archive/0.4.2.tar.gz'
-  sha256 'f61e0aa1f4062175969abdb5c1b9fc054f6b5332213a58f4933c92abc996176a'
-  head 'https://github.com/nrobinson2000/homebrew-po', :using => :git
+  homepage "https://po-util.com"
+  url "https://github.com/nrobinson2000/po/archive/0.4.2.tar.gz"
+  sha256 "f61e0aa1f4062175969abdb5c1b9fc054f6b5332213a58f4933c92abc996176a"
+  head "https://github.com/nrobinson2000/homebrew-po", :using => :git
 
   def install
-    bin.install 'bin/po'
-    share.install 'share/po-common'
-    share.install 'share/po-mac'
-    man1.install 'man/po.1'
+    bin.install "bin/po"
+    share.install "share/po-common"
+    share.install "share/po-mac"
+    man1.install "man/po.1"
     bash_completion.install "completion/po"
   end
 
   def caveats; <<~EOS
-  In order to complete the installation of po-util,
-  you must run 'po install'.
+    In order to complete the installation of po-util,
+    you must run "po install".
 
-  For Raspberry Pi support you must have docker installed.
-  EOS
+    For Raspberry Pi support you must have docker installed.
+    EOS
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test po`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-
-    system "po", "config", "release/v0.6.3", "duo", "false"
-    system "po", "info"
-    system "po", "install"
+    system "#{bin}/po", "config", "release/v0.6.3", "duo", "false"
+    system "#{bin}/po", "info"
+    system "#{bin}/po", "install"
   end
-
 end
