319cbaeeba9ce708641da7f1ae906bceb0282de7
Nathan Robinson <nrobinson2000@me.com>
2018-04-24 00:06:24 -0400

    [skip ci] at 00:06 of 2018-04-24

--- po.rb
+++ po.rb
@@ -1,9 +1,9 @@
 class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage "https://po-util.com"
-  url "https://github.com/nrobinson2000/po/archive/0.4.2.tar.gz"
-  sha256 "f61e0aa1f4062175969abdb5c1b9fc054f6b5332213a58f4933c92abc996176a"
-  head "https://github.com/nrobinson2000/homebrew-po", :using => :git
+  url "https://github.com/nrobinson2000/po/archive/0.4.3.tar.gz"
+  sha256 "e0f1a6bb8daf1b7b64c3c757565471432a97245f6916f7ed1f3260c25119d575"
+  head "https://github.com/nrobinson2000/po", :using => :git
 
   def install
     bin.install "bin/po"
@@ -24,6 +24,5 @@ class Po < Formula
   test do
     system "#{bin}/po", "config", "release/v0.6.3", "duo", "false"
     system "#{bin}/po", "info"
-    system "#{bin}/po", "install"
   end
 end
