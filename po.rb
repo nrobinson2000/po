d0dbad7b385522a03c58d69ecfd51810db3c1117
Nathan Robinson <nrobinson2000@me.com>
2019-01-09 23:25:35 -0500

    default to develop branch at 23:25 of 2019-01-09

--- po.rb
+++ po.rb
@@ -1,8 +1,8 @@
 class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage "https://po-util.com"
-  url "https://github.com/nrobinson2000/po/archive/0.6.5.tar.gz"
-  sha256 "44cc715ea2ab4fd77913f6a58cf647384a6e30af1dda49e4672c555bd2822ca2"
+  url "https://github.com/nrobinson2000/po/archive/0.6.6.tar.gz"
+  sha256 "20bb4dd094f2f3e71ab9fc4562ea723d9f34848c2f3d2e660d8093757dcd8f44"
   head "https://github.com/nrobinson2000/po", :using => :git
 
   def install
