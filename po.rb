3e78c8f6711b7c4281b83a53624ef8c882e356be
Nathan Robinson <nrobinson2000@me.com>
2019-06-11 00:01:35 -0400

    [skip ci]  at 00:01 of 2019-06-11

--- po.rb
+++ po.rb
@@ -1,7 +1,7 @@
 class Po < Formula
   desc "Particle Offline Utility: The Ultimate Local Particle Experience"
   homepage "https://po-util.com"
-  url "https://github.com/nrobinson2000/po/archive/0.6.9.tar.gz"
+  url "https://github.com/nrobinson2000/po/archive/7.0.0.tar.gz"
   sha256 "8f1fc9b3c9ffade82daf0121b72d9e6d03ff914313ee799697ed8e402de333c9"
   head "https://github.com/nrobinson2000/po", :using => :git
 
